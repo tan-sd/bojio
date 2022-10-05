ad73c932b715963ea5ab99fb15851abcf74d7b62
rrachea <rrachelsng@gmail.com>
2022-10-06 03:40:22 +0800

    add eventbrite data in and added to cards

--- script.js
+++ script.js
@@ -115,6 +115,7 @@ window.onload = function() {
   }
 }
 
+//to get event brite data
 const eventName = document.getElementById('eventName');
 fetch(`https://www.eventbriteapi.com/v3/events/366597933287/?token=BXCX5BYSZT4CNPNSUFE2`)
 .then((response) => response.json())
@@ -122,14 +123,106 @@ fetch(`https://www.eventbriteapi.com/v3/events/366597933287/?token=BXCX5BYSZT4CN
   eventName.innerText = data.name.text;
 })
 
-const objectCount = document.getElementById('objectCount');
-fetch(`/raw_response_eventbrite.json`)
+// var objectCount = document.getElementById('objectCount');
+fetch(`json/raw_response_(3).json`)
 .then((response) => response.json())
 .then((data) => {
   // objectCount.innerText = data.pagination.object_count
-  var count = data.pagination.object_count;
-  console.log(count)
-  for (i=0; i < count-1; i++) {
-    objectCount.innerText += data.events[i].name + '\n'
+  // var count = data.pagination.object_count;
+
+  // var inner_div = document.createElement('div')
+  // inner_div.setAttribute('class', 'inner-slider')
+
+  var inner_div = document.getElementsByClassName('inner-slider')[0]
+  console.log(inner_div);
+  
+  var allevents = data.events
+
+  for(indiv_event of allevents){
+    var card = document.createElement('div')
+    card.setAttribute('class', 'card col-4')
+    card.setAttribute('style', 'width: 20rem; height:400px')  
+    // console.log(indiv_event);
+    addthis = ''
+    var name = indiv_event.name
+    var image = indiv_event.image.url
+    var startdate = indiv_event['start_date']
+    var organiser = indiv_event['primary_organizer']['website_url']
+    // console.log(organiser);
+    addthis += `<img src='${image}' class='card-img-top'>
+    <div class='card-body'>
+    <h5 class='card-title'>${name}</h5>
+    <p class='card-text'>${startdate} 
+    <br><br>
+    </p>
+    <a href='${organiser}' target = '_blank' class='btn btn-primary'>Original website</a>
+
+    `
+    console.log(name, image, startdate, organiser);
+    card.innerHTML = addthis
+    console.log(card);
+    inner_div.appendChild(card)
   }
+
+  
+  // var addthis = ''
+  // var tbody = document.getElementsByTagName('tbody')[0]
+  // //loop through object to find key
+  // for( i in objectname){
+  //    var tr = document.createElement('tr')
+  //    var cellone = i
+  //    var celltwo= objectname[i]key
+  //    var cell3 = objectname[i]key
+  //    var cell4 = objectname[i]key
+  //    //var percentage = fit/popln * 100
+  //    //percentage = percentage.toFixed(1)
+  //    tbody.appendChild(tr)
+     
+  //    addthis += ` <tr> 
+  //        <td>${{2:cellone}}</td>
+  //        <td>${celltwo}</td>
+  //        <td>${cell3}</td>
+  //        </tr>`
+  //    tr.innerHTML = addthis 
+  //    tbody.appendChild(tr)
+  //    addthis = ''
+  // console.log(inner_div.innerHTML);
+
+  
+  // console.log(image);
+  // console.log(count)
+  // for (i=0; i < count-1; i++) {
+  //   objectCount.innerText += data.events[i].name + '\n'
+  // }
 })
+
+var div = document.getElementsByClassName('slider-container upper-carousel')[0]
+console.log(div);
+
+
+// inner_div.setAttribute('style', 'width??')
+addthis = ''
+
+
+
+{/* <div class='card' style='width: 18rem;'>
+<img src='./img/emo.png' class='card-img-top'>
+<div class='card-body'>
+  <h5 class='card-title'>Card title</h5>
+  <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href='http://sunjun.site' class='btn btn-primary'>Go somewhere</a>
+</div>
+</div> */}
+
+{/* <div class="slider-container upper-carousel">
+      <h1>Upcoming Events</h1>
+      <div class="inner-slider">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+      </div>
+    </div> */}
