1ffc12127c12bd7c2fca3fb6f9d86cde3066eaae
rrachea <rrachelsng@gmail.com>
2022-10-12 02:19:11 +0800

    update signup n register for test case

--- static/js/register.js
+++ static/js/register.js
@@ -4,7 +4,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebas
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
+import { getDatabase, set, ref, onValue, child, get } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-database.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -43,52 +43,102 @@ var password = document.getElementById('password');
 /* CODE ADDED: END  */
 /* END OF FIREBASE */
 
+//validate email
+function validate_email(email){
+  var expression = /^[^@]+@\w+(\.\w+)+\w$/
+  if(expression.test(email) == true){
+    //ok email
+    return true
 
+  } else{
+    //email invalid
+    return false
+  }
+}
 
-    const db = getDatabase(app);
-    const auth = getAuth();
-    var signupBtn = document.getElementById('signupBtn')
-    signupBtn?.addEventListener('click', (e) => {
-        var email = document.getElementById("email").value;
-        // var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        
-        createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            alert("Successfully signed up!");
-            //Redirect user if you want
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
+const db = getDatabase(app);
+const auth = getAuth();
+var signupBtn = document.getElementById('signupBtn')
+signupBtn?.addEventListener('click', (e) => {
+    var email = document.getElementById("email").value;
+    // var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var status = securityCheck()
+    if (status){
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user);
+          console.log(user.uid);
+          set(ref(db, "accounts/" + user.uid),{
+            firstname: firstName.value,
+            lastname: lastName.value,
+            username: username.value,
+            email: email,
+            password: password,
+            events: ['no events'],
+            createdjios: ['no jios'],
+            // help: 'testthis',
+          })
+          .then(() => {
+            alert('details created successfully!')
+          })
+          .catch((error) => {
+            alert(error);
+          })
+          
+          // alert("Successfully signed up!");
+          //Redirect user if you want
+      })
+      .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+
+          if(errorMessage == 'Firebase: Error (auth/email-already-in-use).'){ 
+            var email = document.getElementById('email')
+            email.classList = "form-control is-invalid";
+            document.getElementById('emailInvalid').innerText = 'Email already exists.'
+
+          }
+          // ..
+          else{
             alert(errorMessage);
-        });
-    })
+
+          }
+      });
+    }
+
+})
 
 
 
 // Retrieve form data and insert it into firebase
-function InsertData() {
-  push(ref(db, "accounts/"),{
-    firstname: firstName.value,
-    lastname: lastName.value,
-    username: username.value,
-    password: password.value
-  })
-  .then(() => {
-    alert('Account created successfully!')
-  })
-  .catch((error) => {
-    alert(error);
-  })
-  }
+// function InsertData() {
+//   push(ref(db, "accounts/"),{
+//     firstname: firstName.value,
+//     lastname: lastName.value,
+//     username: username.value,
+//     password: password.value
+//   })
+//   .then(() => {
+//     alert('Account created successfully!')
+//   })
+//   .catch((error) => {
+//     alert(error);
+//   })
+//   }
+
+
+//do not account for duplicate username yet!!
+//if sign up alr n sign up agn local hosts will throw error
+// Firebase: Error (auth/email-already-in-use).
+//code this and say if this error then error will show up on email box 
 
 // Registration form validation
 function securityCheck(){
     var errorCount = 0;
+    var email = document.getElementById('email')
     var firstName = document.getElementById('firstName');
     var lastName = document.getElementById('lastName');
     var username = document.getElementById('username');
@@ -97,6 +147,7 @@ function securityCheck(){
     var passwordInvalidError = document.getElementById('passwordInvalid');
     var passwordValidation = false;
 
+    var emailstatus = validate_email(email.value)
     // function containsSpecialChars(str) {
     // const specialChars =
     //   '[`!@#$%^&*()_+-=[]{};\':"\\|,.<>/?~]/';
@@ -106,14 +157,26 @@ function securityCheck(){
     // }
     // console.log(containsSpecialChars(username));
     
+    if(email.value.length == 0) {
+      email.classList = "form-control is-invalid";
+      errorCount += 1;
+
+    } else {
+      email.classList = "form-control is-valid";
+    }
+
     if(firstName.value.length == 0) {
       firstName.classList = "form-control is-invalid";
+      errorCount += 1;
+
     } else {
       firstName.classList = "form-control is-valid";
     }
     
     if(lastName.value.length == 0) {
       lastName.classList = "form-control is-invalid";
+      errorCount += 1;
+
     } else {
       lastName.classList = "form-control is-valid";
     }
@@ -153,38 +216,57 @@ function securityCheck(){
 
     // var userexist = false;
     var db_user  = ''
+    // var userexist = false 
     if (errorCount == 0) {
+      // return true
       //check if username alr exists
       var userexist = false;
   
-        
-        const getusers = ref(db, 'accounts/');
-        onValue(getusers, (snapshot) => {
-        const data = snapshot.val();
-        console.log(data);
-        for(var i in data){
-          var userexist = false;
-          db_user = data[i]['username']
-          console.log(username.value);
-          console.log(db_user);
-          if(username.value == db_user){
-            console.log('user exist');
-            username.classList = "form-control is-invalid";
-            username.innerText = 'Username already exists.'
-            var userexist = true
-            break;
+        // const getusers = ref(db, 'accounts/');
+
+        const dbRef = ref(getDatabase());
+        get(child(dbRef, `accounts/`)).then((snapshot) => {
+          console.log(snapshot);
+          if (snapshot.exists()) {
+            console.log(snapshot.val());
+          } else {
+            console.log("No data available");
           }
-        }
+        }).catch((error) => {
+          console.error(error);
+        });
+
+
+        // onValue(getusers, (snapshot) => {
+        // const data = snapshot.val();
+        // console.log(data);
+        // for(var i in data){
+        //   var userexist = false;
+        //   db_user = data[i]['username']
+        //   console.log(username.value);
+        //   console.log(db_user);
+        //   if(username.value == db_user){
+        //     console.log('user exist');
+        //     username.classList = "form-control is-invalid";
+        //     username.innerText = 'Username already exists.'
+        //     var userexist = true
+        //     break;
+        //   }
+        // }
 
         console.log(userexist);
         if(!userexist){
-            console.log(userexist);
-            console.log(db_user);
-            console.log('hi');
-            InsertData();
+      //       console.log(userexist);
+      //       console.log(db_user);
+      //       console.log('hi');
+      //       InsertData();
+
+          //means user dont exist n its gd
+          return true
           }
+        return false
       
-      });
+      
       
       
       
