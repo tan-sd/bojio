6570b2a1c5335acfd581a6467727b7acf7148769
mffoo13 <fooyaoshengmatthew@gmail.com>
2022-10-15 16:11:46 +0800

    New change

--- static/js/createajio.js
+++ static/js/createajio.js
@@ -1,12 +1,14 @@
 
 var placeholder=document.getElementById('result');
 var activityArr=[];
-
 var totalDuration=0;
-if (totalDuration==0){
+
+    
+if (activityArr.length==0){
   var output='<h5>You have no activities yet</h5>'
   placeholder.innerHTML=output;
 } 
+
 
 
 
@@ -23,8 +25,6 @@ function update(){
   if (isNaN(duration)){
     alert('Please enter a valid duration');
   }
-
-
 
   else{
     var activity = {
@@ -52,18 +52,26 @@ function removeActivity(item){
         var timeRemoved=parseInt(act.duration);
         totalDuration-=timeRemoved
         var ind=activityArr.indexOf(act);
+        activityArr.splice(ind,1);
         break
       }
     }
-    activityArr.splice(ind,1);
-  
-    activityTable=`<table><tr><th>Number</th><th>Name</th><th>Location</th><th>Duration</th><th></th></tr>`
-    for(i=0;i<activityArr.length;i++){
-      activityTable+=`<tr><td>${i+1}</td><td>${activityArr[i].name}</td><td>${activityArr[i].location}</td><td>${activityArr[i].duration}</td><td><button type="button" value="${activityArr[i].name}" onclick="removeActivity(this)">Delete</button></td></tr>`
+
+    if (activityArr.length==0){
+      var output='<h5>You have no activities yet</h5>'
+      placeholder.innerHTML=output;
+    } 
+
+    else{
+      activityTable=`<table><tr><th>Number</th><th>Name</th><th>Location</th><th>Duration</th><th></th></tr>`
+      for(i=0;i<activityArr.length;i++){
+        activityTable+=`<tr><td>${i+1}</td><td>${activityArr[i].name}</td><td>${activityArr[i].location}</td><td>${activityArr[i].duration}</td><td><button type="button" value="${activityArr[i].name}" onclick="removeActivity(this)">Delete</button></td></tr>`
+      }
+      placeholder.innerHTML=activityTable+`</table><br><br><p>Event's total duration: ${totalDuration} mins</p>`;
+
     }
-    placeholder.innerHTML=activityTable+`</table><br><br><p>Event's total duration: ${totalDuration} mins</p>`;
-    
 }
+
 
 
 
