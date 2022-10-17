90c8e2b4a3fd1f4824afe4f45805db7de994e366
rrachea <rrachelsng@gmail.com>
2022-10-17 09:58:56 +0800

    sorry commented out functions

--- wad2-project-vue/src/utils/index.js
+++ wad2-project-vue/src/utils/index.js
@@ -38,105 +38,105 @@ export function getdata(){
 
 
 
-var eventname = ''
-var username =''
-var date =''
-var type = ''
-var activities =''
-var testthis = ''
+// var eventname = ''
+// var username =''
+// var date =''
+// var type = ''
+// var activities =''
+// var testthis = ''
 
-//to createjio
-function createJio(eventname, type) {
-  const db = getDatabase();
+// //to createjio
+// function createJio(eventname, type) {
+//   const db = getDatabase();
   
-  eventname = document.getElementById('name').value
+//   eventname = document.getElementById('name').value
 
-  if (typeof(Storage) !== "undefined") {
-    username = localStorage.getItem('fullname')}
-  else{
-    username = 'nousername'
-  }
+//   if (typeof(Storage) !== "undefined") {
+//     username = localStorage.getItem('fullname')}
+//   else{
+//     username = 'nousername'
+//   }
 
-  date = document.getElementById('date').value
-  type = document.querySelector('input[name="exampleRadios"]:checked').value;
+//   date = document.getElementById('date').value
+//   type = document.querySelector('input[name="exampleRadios"]:checked').value;
 
-  const jioData = {
-    // creator: username,
-    eventname: eventname,
-    username: username,
-    date: date,
-    type: type,
-    activities: [1,2,3],
-  };
+//   const jioData = {
+//     // creator: username,
+//     eventname: eventname,
+//     username: username,
+//     date: date,
+//     type: type,
+//     activities: [1,2,3],
+//   };
 
-  // Get a key for a new Post.
-  const newKey = push(child(ref(db), 'events')).key;
+//   // Get a key for a new Post.
+//   const newKey = push(child(ref(db), 'events')).key;
 
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  const updates = {};
+//   // Write the new post's data simultaneously in the posts list and the user's post list.
+//   const updates = {};
 
-  //to separate public n private events
-  // privateevents 
-  //      userid
-  //        -jio data
+//   //to separate public n private events
+//   // privateevents 
+//   //      userid
+//   //        -jio data
 
 
-  updates[`${type} events/${uid}/${newKey}`] = jioData;
+//   updates[`${type} events/${uid}/${newKey}`] = jioData;
 
-  // accounts
+//   // accounts
 
-  // eventsgoing
+//   // eventsgoing
               
-  // createdjios
+//   // createdjios
               
-  updates['/createdjios/' + uid + '/' + newKey] = jioData;
+//   updates['/createdjios/' + uid + '/' + newKey] = jioData;
 
 
-  //add under createdjios which is under username account
-  updates[`accounts/${uid}/createdjios/${newKey}`] = jioData;
+//   //add under createdjios which is under username account
+//   updates[`accounts/${uid}/createdjios/${newKey}`] = jioData;
 
-  console.log(updates);
-  return update(ref(db), updates);
-}
+//   console.log(updates);
+//   return update(ref(db), updates);
+// }
 
 
-//see private jios
-var privatejios = document.getElementById('private')
-privatejios.addEventListener('click', getprivate)
+// //see private jios
+// var privatejios = document.getElementById('private')
+// privatejios.addEventListener('click', getprivate)
 
-const dbRef = ref(getDatabase());
+// const dbRef = ref(getDatabase());
 
-function getprivate(){
-get(child(dbRef, `private events/`)).then((snapshot) => {
-  if (snapshot.exists()) {
+// function getprivate(){
+// get(child(dbRef, `private events/`)).then((snapshot) => {
+//   if (snapshot.exists()) {
 
-    localStorage.setItem('privatejios', JSON.stringify(snapshot.val()))
+//     localStorage.setItem('privatejios', JSON.stringify(snapshot.val()))
    
-  } else {
-    console.log("No data available");
-  }
-}).catch((error) => {
-  console.error(error);
-});
-}
+//   } else {
+//     console.log("No data available");
+//   }
+// }).catch((error) => {
+//   console.error(error);
+// });
+// }
 
 
-//get public 
-var publicjios = document.getElementById('public')
-publicjios.addEventListener('click', getpublic)
+// //get public 
+// var publicjios = document.getElementById('public')
+// publicjios.addEventListener('click', getpublic)
 
-function getpublic(){
-  get(child(dbRef, `public events/`)).then((snapshot) => {
-    if (snapshot.exists()) {
+// function getpublic(){
+//   get(child(dbRef, `public events/`)).then((snapshot) => {
+//     if (snapshot.exists()) {
   
-      // console.log(snapshot.val());
-      localStorage.setItem('publicjios', JSON.stringify(snapshot.val()))
+//       // console.log(snapshot.val());
+//       localStorage.setItem('publicjios', JSON.stringify(snapshot.val()))
 
-      // localStorage.setItem('publicjios', JSON.stringify(snapshot.val())
-    } else {
-      console.log("No data available");
-    }
-  }).catch((error) => {
-    console.error(error);
-  });
-  }
+//       // localStorage.setItem('publicjios', JSON.stringify(snapshot.val())
+//     } else {
+//       console.log("No data available");
+//     }
+//   }).catch((error) => {
+//     console.error(error);
+//   });
+//   }
