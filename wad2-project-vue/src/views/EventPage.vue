1fb39c41c9f6aa81bb7acf2dee533404c4b5f869
faezlatiff <faezlatiff0706@gmail.com>
2022-11-05 03:24:16 +0800

    added images & edited max-width EventPage card

--- wad2-project-vue/src/views/EventPage.vue
+++ wad2-project-vue/src/views/EventPage.vue
@@ -59,7 +59,7 @@
             <div id="event-banner-card" class="col-11 col-sm-10 col-md-8 col-lg-6">
               <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false">
                 <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#868e96"></rect>\
+                <rect width="100%" height="100%" fill="#868e96"></rect>
               </svg>
                 <div id="event-banner-card-body">
                     <div class="row">
